3e41af06f359d4bc0713fa550dcd1c6d89c9a0b9
Jarrod Cuzens <jcuzens@gmail.com>
2021-08-10 17:39:03 -0700

    More tweaks because the environment variable didn't get set properly

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 # - GPG_KEY_NAME
 # - GPG_PASSPHRASE
 # expects file to exist:
-# - .travis/gpg.key
+# - gpg.key
 
 set -e
 
@@ -18,22 +18,22 @@ fi
 
 if [ -z "$OSSRH_PASSWORD" ]; then
   echo "missing environment value: OSSRH_PASSWORD" >&2
-  exit 1
+  exit 2
 fi
 
 if [ -z "$GPG_KEY_NAME" ]; then
   echo "missing environment value: GPG_KEY_NAME" >&2
-  exit 1
+  exit 3
 fi
 
-#if [ -z "$GPG_PASSPHRASE" ]; then
-#  echo "missing environment value: GPG_PASSPHRASE" >&2
-#  exit 1
-#fi
+if [ -z "$GPG_PASSPHRASE" ]; then
+  echo "missing environment value: GPG_PASSPHRASE" >&2
+  exit 4
+fi
 
 # Prepare the local keyring (requires travis to have decrypted the file
 # beforehand)
-#gpg --fast-import gpg.key
+gpg --fast-import gpg.key
 
 # Prepare the m2 settings
 cp .travis.settings.xml $HOME/.m2/settings.xml
