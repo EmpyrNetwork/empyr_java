3bedde7283b242bec4e1acf4f12f2a3f3e4f7e07
Jared Cooper <jcooper@gofigg.com>
2024-01-25 13:20:14 -0700

    CD-1955 add dateOfApproval to api response

--- src/main/java/com/empyr/api/model/RestBusiness.java
+++ src/main/java/com/empyr/api/model/RestBusiness.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.empyr.api.model;
 
@@ -18,97 +18,100 @@ import com.empyr.api.model.RestCard.CardType;
  */
 @Documented( name="venue", description="Represents a venue/location/business. Includes all fields of the compactVenue." )
 public class RestBusiness extends RestCompactBusiness
-{	
+{
 	@ApiField( value="The category of the business.", optional=true )
-	public String primaryCategory;	
-	
+	public String primaryCategory;
+
 	@ApiField( "A list of all the business categories." )
 	public String[] categories;
-	
+
 	@ApiField( "Whether this business is in the users bookmarks. Only populated on the Venue GET call" )
 	public Boolean bookmarked;
-	
+
 	@ApiField( "The website of the venue." )
 	public String website;
-	
+
 	@ApiField( value="A description of the venue.", optional=true )
 	public String description;
-	
+
 	@ApiField( value="Represents the price range of the venue.", optional=true )
 	public String priceRange;
-	
+
 	@ApiField( "The menu for the venue." )
 	public RestUrl menuUrl;
-	
+
 	@ApiField( "Address of the venue." )
 	public RestMedia[] medias;
-	
+
 	@ApiField( "Consumer buzz associated with the venue." )
 	public String [] buzz;
-	
+
 	@ApiField( "Various features that the venue offers." )
 	public String [] features;
-	
+
 	@ApiField( "The noise level expected at this venue." )
 	public String [] noiseLevel;
-	
+
 	@ApiField( "The kind of ambiance the venue provides." )
 	public String [] ambiance;
-	
+
 	@ApiField( "Whether the venue serves breakfast, lunch, dinner." )
 	public String [] serves;
-	
+
 	@ApiField( "The kind of acceptable attire at the venue." )
 	public String [] attire;
-	
+
 	@ApiField( "The accepted payment types at the venue." )
 	public String [] acceptedCards;
-	
+
 	@ApiField( value="What they're known for.", nullable=true )
 	public String knownFor;
-	
+
 	@ApiField( "Best nights to go to this venue." )
 	public String [] bestNights;
-	
+
 	@ApiField( "Olson timezone of the business." )
 	public String timezone;
-	
+
 	@ApiField( "Timezone offset from GMT." )
 	public int timezoneOffset;
-	
+
 	@ApiField( value="A simple representation of the hours of operation", optional=true )
 	public Map<String,String> simpleHours = new LinkedHashMap<String,String>();
-	
+
 	@ApiField( "The default reward percentage for this business." )
 	public double defaultDiscount;
-	
+
 	@ApiField( value="A list of all the current day's variable rewards offers at the business", type=RestVariableReward.class )
 	public List<RestVariableReward> rewards = new ArrayList<RestVariableReward>();
-	
+
 	@ApiField( value="A list of the complete rewards schedule for the business", type=RestVariableReward.class )
 	public Map<String,List<RestVariableReward>> rewardsSchedule = new LinkedHashMap<String,List<RestVariableReward>>();
-	
+
 	@ApiField( value="Indicates whether the business is open or closed currently.")
 	public Boolean closed;
-	
+
 	@ApiField( value="State of the business" )
 	public String status;
-	
+
+	@ApiField( value="Date of approval" )
+	public Date dateOfApproval;
+
 	@ApiField( value="A list of offers that this business is offering.", type=RestOffer.class)
 	public List<RestOffer> offers = new ArrayList<RestOffer>();
-	
+
 	@ApiField( value="This offer should be published" )
 	public boolean publish;
-	
+
 	@ApiField( value="Current day (business local time). Expressed as [SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY]" )
 	public String businessLocalDay;
-	
+
 	@ApiField( value="Current time (business local time). Expressed as HHMM" )
 	public String businessLocalTime;
-	
+
 	@ApiField( value="The chain that this business belongs to.", optional=true )
 	public RestChain chain;
-	
+
 	@ApiField( value="The merchant info for the venue.", optional=true)
 	public RestMerchantInfo merchantInfo;
 }
